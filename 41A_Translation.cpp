41c835c2640d413f78a61d8117a9336b7fc2c840
Deepansh Chandra <deech.honey@gmail.com>
2026-08-06 08:55:56 +0530

    Codeforces solutions

---
+++ 41A_Translation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s,t;
+    cin>>s;
+    cin>>t;
+
+    for()
+    return 0;
+}
---
+++ 50A_Domino_Piling.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int div,m,n;
+    cin>>m>>n;
+    div= (m*n)/2;
+    cout<<div;
+
+    return 0;
+}
